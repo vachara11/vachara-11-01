bc362ef92cca208fb7c9fa7c2d07caa7e5107edd
Watchara Ketkaew <wk996110@gmail.com>
2022-07-22 11:10:47 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header"><h2><b>My Profile</b></h2></div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                    <p><h4><b>Name : </b> {{ Auth::user()->name }} </h4></p>
+                    <p><h4><b>Email : </b> {{ Auth::user()->email }} </h4></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
